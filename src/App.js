389b8803caa5484ad7bf7aa4d6585ac1c3ca2a29
Mies Eliel Dela Cruz <mieseliel.delacruz@gmail.com>
2021-09-13 23:45:45 +0800

    Add sections and component imports

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
-import Navbar from "./Components/Navbar/navbar";
+import Navbar from "./components/navbar/Navbar";
+import Landing from "./components/landing/Landing";
+import Portfolio from "./components/portfolio/Portfolio";
+import Works from "./components/works/Works";
+import Contact from "./components/contact/Contact";
+import "./app.scss";
 
 function App() {
   return (
     <div className="App">
       <Navbar/>
-      
+      <div className="sections">
+        <Landing/>
+        <Portfolio/>
+        <Works/>
+        <Contact/>
+      </div>
     </div>
   );
 }
